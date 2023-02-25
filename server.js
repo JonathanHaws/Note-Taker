0bf8b8cfa6a0adba62d6489c406113cbc726ffa3
Jonathan Haws <108207472+JonathanHaws@users.noreply.github.com>
2023-02-25 13:39:56 -0700

    fix annoying typo

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app         = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.get('/',(req, res)=> res.sendFile(path.join(__dirname,'/public/index.html')));
+app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'/public/index.html')));
 app.get('/notes',(req,res)=> res.sendFile(path.join(__dirname,'/public/notes.html')));
 app.get('/api/notes',(req,res)=> res.json(JSON.parse(fs.readFileSync(path.join(__dirname,'/db/db.json'),'utf8'))));
 app.post('/api/notes',(req,res)=>{ 
